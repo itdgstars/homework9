d053ac53c185739aa1a5abbd3c92fa5b73bfbf78
SergeyK <it-dgstars@mail.ru>
2023-03-09 09:35:51 +0300

    решили задачу 66

---
+++ z66/Program.cs
@@ -0,0 +1,23 @@
+﻿int SumDigits (int m, int n)
+{   
+    
+    if (m < 0) m *= -1;
+   
+    if (m <= n)
+    {
+        
+        return SumDigits (m+1,n) + m;
+    } 
+    
+    else return 0;
+   
+    
+    
+}
+Console.WriteLine("Введите число M");
+int muser = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите число N");
+int nuser = Convert.ToInt32(Console.ReadLine ());
+
+int res = SumDigits (muser, nuser);
+Console.WriteLine(res);
