5f60aa49a71711d5a9405c184f30b99ae0aa20fc
SergeyK <it-dgstars@mail.ru>
2023-03-09 09:36:56 +0300

    решили задачу 68

---
+++ z68/Program.cs
@@ -0,0 +1,27 @@
+﻿int AkkFun(int m, int n)
+{
+  if (m < 0 || n < 0)
+  {
+    Console.WriteLine ("Одно или оба ваших чисел ОТРИЦТЕЛЬНЫЕ!");
+  }
+  else {
+  if (m == 0)
+    return n + 1;
+  else
+    if ((m != 0) && (n == 0))
+      return AkkFun(m - 1, 1);
+    else
+      return AkkFun(m - 1, AkkFun(m, n - 1));
+      
+}
+return 0;
+}
+
+Console.Write ("Введите число М: ");
+int userm = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число N: ");
+int usern = Convert.ToInt32(Console.ReadLine());
+
+int res = AkkFun(userm, usern);
+Console.WriteLine(res);
+
