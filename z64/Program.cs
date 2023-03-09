2c07e4b4a402ab4137af51611c63d0a36791a04b
SergeyK <it-dgstars@mail.ru>
2023-03-09 09:34:52 +0300

    решили задачу 64

---
+++ z64/Program.cs
@@ -0,0 +1,12 @@
+﻿void ShowNumbers(int number)
+{
+    Console.Write(number + " ");
+    if (number > 1) ShowNumbers(number - 1);
+    
+
+    
+}
+Console.Write("Укажите Ваше число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+ShowNumbers(numN);
+
